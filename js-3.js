4f96129f11ca35bcb2e0053bbe2db7cbe24b1f28
AlesiaKolbik <32718331+AlesiaKolbik@users.noreply.github.com>
2018-06-06 19:10:45 +0300

    ++

---
+++ js-3.js
@@ -0,0 +1,103 @@
+'use strict';
+
+const readline = require('readline');
+
+const result = {
+    firstName:'',
+    secondName:'',
+    patronymic:'',
+    age:0,
+    gender:'',
+    retiree:''
+};
+askSecondName();
+function askSecondName() {
+    const rl = prompt();
+    rl.question('Введите вашу фамилию:', (answer) =>{
+        rl.close();
+        if(!answer){
+            console.log('Вы ничего не ввели.Повторите ввод:');
+            askSecondName();
+        }
+        else {
+            result.secondName = answer;
+            askName();
+        }
+    });
+}
+function askName() {
+    const rl = prompt();
+    rl.question('Введите Ваше имя: ', (answer) => {
+        rl.close();
+        if (!answer) {
+            console.log('Имя не может быть пустым.');
+            askName();
+        }
+        else {
+            result.firstName = answer;
+            askPatronymic();
+        }
+    });
+}
+
+function askPatronymic() {
+    const rl = prompt();
+    rl.question('Введите Ваше отчество:', (answer) =>{
+        rl.close();
+        if(!answer){
+            console.log('Вы ничего не ввели. Повторите ввод:');
+            askPatronymic();
+        }
+        else {
+            result.patronymic = answer;
+            askAge();
+        }
+    });
+
+}
+function askAge() {
+    const rl = prompt();
+    rl.question('Введите Ваш возраст цифрами: ', (answer) => {
+        rl.close();
+        const age = parseFloat(answer);
+        if (age<= 0 || age > 120 || isNaN(age)) {
+            console.log('Возраст введен некорректно. Повторите ввод:');
+            askAge();
+        }
+        else {
+            result.age = age;
+            askGender();
+        }
+    });
+}
+function askGender() {
+    const rl = prompt();
+    rl.question('Ваш пол (м/ж)?', (answer) =>{
+        rl.close();
+        if(answer === 'м' || answer === 'муж' || answer === 'мужской'){
+            result.gender = 'мужской';
+            end();
+        }
+        else if(answer === 'ж' || answer === 'жен' || answer === 'женский'){
+            result.gender = 'женский';
+            end();
+        }
+        else {
+          console.log('Вы ввели пол не корректно. Введите м или ж:');
+          askGender();
+        }
+    });
+}
+
+function prompt() {
+    return readline.createInterface({ input: process.stdin,  output: process.stdout});
+}
+
+function end() {
+    console.log(`Ваше ФИО:${result.secondName} ${result.firstName} ${result.patronymic};
+     Ваш возраст: ${result.age};
+     Ваш возраст в днях:${result.age *365};
+     Через 5 лет вам будет:${result.age +5};
+     Ваш пол: ${result.gender};
+     Вы на пенсии: ${result.age>60?"да":"нет"}`);
+}
